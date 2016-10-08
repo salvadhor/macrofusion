13ebf27bd956d464760da0c6e928b345abcfd945
Philipp Lutz <philipp.lutz@gmx.de>
2016-10-08 02:53:24 +0200

    improved style and readability

--- macrofusion.py
+++ macrofusion.py
@@ -2,11 +2,11 @@
 # -*- coding: utf-8 -*-
 # License : GPLv3 : http://gplv3.fsf.org/
 
-APP = 'MacroFusion'
-__VERSION__='0.7.5'
-__LICENSE__='GPL'
-__COPYRIGHT__='Dariusz Duma'
-__WEBSITE__='http://sourceforge.net/p/macrofusion'
+APP             = 'MacroFusion'
+__VERSION__     ='0.7.5'
+__LICENSE__     ='GPL'
+__COPYRIGHT__   ='Dariusz Duma'
+__WEBSITE__     ='http://sourceforge.net/p/macrofusion'
 
 try:
 
@@ -44,12 +44,6 @@ except:
     print('Python Imaging Library is missing.')
 
 
-# Bad, bad, really bad coder... Global variables...
-global session_images_bak
-session_images_bak=[]
-global session_options_bak
-session_options_bak=[]
-
 if os.path.exists('/usr/share/mfusion/ui/ui.xml') \
     and os.path.exists('/usr/share/mfusion/ui/progress.xml') \
     and os.path.exists('/usr/share/pixmaps/macrofusion.png') \
@@ -84,7 +78,7 @@ def toggled_cb(cell, path, user_data):
 
 # PLEASE REAPAIR!! Python-imaging can't open .tiff (or some of them)    
 def create_thumbnail(chemin,taille):
-    outfile=settings["preview_folder"] + '/' + os.path.split(chemin)[1]
+    outfile = settings["preview_folder"] + '/' + os.path.split(chemin)[1]
     try:
         im = GdkPixbuf.Pixbuf.new_from_file_at_size(chemin, taille[0], taille[1])
 #        pb = Gtk.gdk.pixbuf_new_from_file(chemin)
@@ -661,7 +655,7 @@ class Interface:
             settings["fuse_settings"]["image_cacheblocksize"][1] = self.spinbuttonbloc.get_value_as_int()
 
         if not self.checkbuttontaillefinale.get_active():
-            settings["fuse_settings"]["output_dimensions"] = ["-f",
+            settings["fuse_settings"]["output_dimensions"] = [ "-f",
                                                     str(self.spinbuttonlargeurfinale.get_value_as_int()) + 'x'
                                                     + str(self.spinbuttonhauteurfinale.get_value_as_int()) + 'x'
                                                     + str(self.spinbuttonxoff.get_value_as_int()) + 'x'
@@ -696,15 +690,15 @@ class Interface:
         if len(self.liststoreimport) <= 1:
             self.messageinthebottle(_("Please add or activate at least two images.\n\n Cannot do anything smart with the one or no image."))
             return
-        FenPar=SaveFiles_Dialog(self.win)
+        FenPar = SaveFiles_Dialog(self.win)
         self.name = FenPar.get_name()
         if self.name:
             if not re.search('\\.jpeg$|\\.jpg$|\\.tiff$|\\.tif$', self.name, flags=re.IGNORECASE):
-                self.name+=".jpg"
+                self.name += ".jpg"
             self.start('')
     
     def sendto(self, widget):
-        self.name=(settings["preview_folder"] + "/sendto.tif")
+        self.name = (settings["preview_folder"] + "/sendto.tif")
         if not os.path.exists(self.name):
             self.messageinthebottle(_("Please add or activate at least two images.\n\n Cannot do anything smart with one or no image."))
             return
@@ -720,7 +714,7 @@ class Interface:
             self.messaga.destroy()
 
     def get_exif(self, file):
-        tags2=''
+        tags2 = ''
         try:
              im = GExiv2.Metadata(file)
              tags_keys = im.get_exif_tags()
@@ -739,9 +733,9 @@ class Interface:
         return tags2
                
     def start(self, issend):        
-        self.issend=issend
-        self.liste_images=[]
-        self.liste_aligned=[]
+        self.issend = issend
+        self.liste_images = []
+        self.liste_aligned = []
         index = 0
         for item in self.liststoreimport:
             if item[0]:
@@ -793,25 +787,25 @@ class Interface:
         return Image.frombytes("RGB",(width,height),pb.get_pixels() )
 
     def put_files_to_the_list(self, files):
-        self.files=files
-        self.tags2=''
-        self.badfiles=[]
+        self.files = files
+        self.tags2 = ''
+        self.badfiles = []
         for file in self.files:
             if re.search('\\.jpg$|\\.jpeg$|\\.tiff$|\\.tif$', file, flags=re.IGNORECASE):
                 pb = GdkPixbuf.Pixbuf.new_from_file(file)
                 im = self.pixbuf2Image(pb)
-                self.size=im.size
+                self.size = im.size
                 # self.tags2 = Gui.get_exif(file)
                 if not self.tags2:
-                    self.tags2=''
-                self.tooltip=("\n" + _("<b>Filename:</b> ") + os.path.basename(file) + "\n"+_("<b>Resolution:</b> ") + str(str(self.size[0]) + "x" + str(self.size[1])) + "\n" + self.tags2)
+                    self.tags2 = ''
+                self.tooltip = ("\n" + _("<b>Filename:</b> ") + os.path.basename(file) + "\n"+_("<b>Resolution:</b> ") + str(str(self.size[0]) + "x" + str(self.size[1])) + "\n" + self.tags2)
                 self.liststoreimport.append([1, file, GdkPixbuf.Pixbuf.new_from_file_at_size(file, 128, 128), self.tooltip])
             else:
                 self.badfiles.append(file)
         if len(self.badfiles)>0:
-            message=_("Only JPEG and TIFF files are allowed.\n\nCannot open:\n")
+            message = _("Only JPEG and TIFF files are allowed.\n\nCannot open:\n")
             for itz in self.badfiles:
-                message+=itz + "\n"
+                message += itz + "\n"
             Gui.messageinthebottle(message)
         return 
         
@@ -823,10 +817,10 @@ class OpenFiles_Dialog:
     """La classe qui ouvre la fenetre de choix de files, et qui retourne le ListStore par la methode get_model"""
     def __init__(self, model, parent):
         """Lance la fenetre de selection et créé la listsore a partir des files selectionnés"""
-        self.filter=Gtk.FileFilter()
+        self.filter = Gtk.FileFilter()
         self.filter.add_mime_type("image/jpeg")
         self.filter.add_mime_type("image/tiff")
-        self.liststoreimport=model #on repart de l'ancien modele
+        self.liststoreimport = model #on repart de l'ancien modele
 
         self.file_dialog = Gtk.FileChooserDialog(_("Add images..."), 
                                                     parent, 
@@ -842,8 +836,8 @@ class OpenFiles_Dialog:
                  
         if (self.file_dialog.run() == Gtk.ResponseType.OK):
             self.files = self.file_dialog.get_filenames()
-            self.tags2=''
-            self.badfiles=[]
+            self.tags2 = ''
+            self.badfiles = []
             (path, file) = os.path.split(self.files[0])
             (filename, ext) = os.path.splitext(file)
             settings["default_file"] = filename+"-fused"+ext
@@ -907,36 +901,33 @@ class SaveFiles_Dialog:
 class Thread_Preview(threading.Thread):
     def __init__(self, taille, liste):
         threading.Thread.__init__ (self)
-        self.taille=taille
-        self.liste=liste
+        self.taille = taille
+        self.liste = liste
             
     def run(self):
-        images_a_fusionner=[]
-        images_a_align=[]
+        images_a_fusionner = []
+        images_a_align = []
         index = 0
-        global session_images_bak
-        global session_options_bak
         for item in self.liste:
             if item[0]:
-                chemin_miniature=create_thumbnail(item[1],(int(self.taille[0]), int(self.taille[1])))
+                chemin_miniature = create_thumbnail(item[1],(int(self.taille[0]), int(self.taille[1])))
                 images_a_align.append(chemin_miniature)
                 images_a_fusionner.append(settings["preview_folder"] + "/test" + format(index, "04d") + ".tif")
                 index += 1
-        if (len(images_a_fusionner))<=1:
+        if (len(images_a_fusionner)) <= 1:
             Gui.messageinthebottle(_("Please add two or more images.\n\n Cannot do anything smart with the one image."))
             return
         if not Gui.checkbutton_a5_align.get_active():
             images_a_fusionner = images_a_align
         if Gui.checkbutton_a5_align.get_active():
-            command=["align_image_stack", "-a", settings["preview_folder"] + "/test"] + data.get_align_options() + images_a_align
+            command = ["align_image_stack", "-a", settings["preview_folder"] + "/test"] + data.get_align_options() + images_a_align
             Gui.statusbar.push(15, _(":: Align photos..."))
-            preview_process=subprocess.Popen(command, stdout=subprocess.PIPE)
+            preview_process = subprocess.Popen(command, stdout=subprocess.PIPE)
             preview_process.wait()
-            session_images_bak=images_a_align
             Gui.statusbar.pop(15)
         Gui.statusbar.push(15, _(":: Fusing photos..."))
-        command=[settings["enfuser"], "-o", settings["preview_folder"] + "/" + "preview.tif"] + data.get_enfuse_options() + images_a_fusionner
-        preview_process=subprocess.Popen(command, stdout=subprocess.PIPE)
+        command = [settings["enfuser"], "-o", settings["preview_folder"] + "/" + "preview.tif"] + data.get_enfuse_options() + images_a_fusionner
+        preview_process = subprocess.Popen(command, stdout=subprocess.PIPE)
         preview_process.wait()
         Gui.statusbar.pop(15)
         
@@ -988,15 +979,15 @@ class Progress_Fusion:
 class Thread_Fusion(threading.Thread):
     def __init__(self, command_fuse, command_align, liste, liste_aligned, issend):
         threading.Thread.__init__ (self)
-        self.command_fuse =  [settings["enfuser"], "-o", self.name] + data.get_align_options() + self.liste_aligned
-        self.command_align = ['align_image_stack', '-a', settings["preview_folder"] + '/' + settings.align_prefix] + data.get_align_options() + self.liste_images
+        self.command_fuse  = [settings["enfuser"], "-o", self.name] + data.get_align_options() + self.liste_aligned
+        self.command_align = ["align_image_stack", '-a', settings["preview_folder"] + '/' + settings.align_prefix] + data.get_align_options() + self.liste_images
         self.issend = issend
-        self.liste = liste
+        self.liste  = liste
         self.liste_aligned = liste_aligned
         
     def run(self):
         if Gui.checkbutton_a5_align.get_active():            
-            align_process=subprocess.Popen(self.command_align, stdout=subprocess.PIPE)
+            align_process = subprocess.Popen(self.command_align, stdout=subprocess.PIPE)
             align_process.wait()
         
             if Gui.checkbuttonalignfiles.get_active():
@@ -1020,9 +1011,9 @@ class Thread_Fusion(threading.Thread):
                     if os.path.exists(new_filename_dst):
                         os.remove(new_filename_dst)
                     shutil.move(new_filename, new_filename_dst)
-                    count+=1
+                    count += 1
             
-        fusion_process=subprocess.Popen(self.command_fuse, stdout=subprocess.PIPE)
+        fusion_process = subprocess.Popen(self.command_fuse, stdout=subprocess.PIPE)
         fusion_process.wait()
         
         if Gui.checkbuttonexif.get_active():
@@ -1046,7 +1037,7 @@ class AproposFen:
         self.aboutdialog.set_comments('A GTK Gui for the excellent Enfuse.\n\n2014 (c) Dariusz Duma\n<dhor@toxic.net.pl>')
         # self.aboutdialog.set_copyright(__COPYRIGHT__)
         self.aboutdialog.set_website(__WEBSITE__)
-        self.pixbuf=GdkPixbuf.Pixbuf.new_from_file(IMG + "macrofusion.png")
+        self.pixbuf = GdkPixbuf.Pixbuf.new_from_file(IMG + "macrofusion.png")
         self.aboutdialog.set_logo(self.pixbuf)
         self.aboutdialog.connect("response", self.close_about)
         self.aboutdialog.show()
@@ -1065,9 +1056,9 @@ if __name__ == "__main__":
     Gui  = Interface()
                                
     if (len(sys.argv)>1):     
-        files=sys.argv[1:]
+        files = sys.argv[1:]
         Gui.put_files_to_the_list(files)
-#        if len(Gui.liststoreimport)==0:
+#        if len(Gui.liststoreimport) == 0:
 #            Gui.messageinthebottle(_("\nCan work only with JPEG or TIFF files."))
 
     Gtk.main()
